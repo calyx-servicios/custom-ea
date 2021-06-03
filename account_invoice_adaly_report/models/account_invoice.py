8a3822071c41331f5203e83ae0234e04f6f2e7a4
JhoneM <jhone0901@gmail.com>
2021-06-03 16:04:44 -0300

    [UPD][account_invoice_adaly_report]  - New field in account_invoice  - Condition to show button  - Change in template report

--- account_invoice_adaly_report/models/account_invoice.py
+++ account_invoice_adaly_report/models/account_invoice.py
@@ -1,13 +1,33 @@
 # Copyright 2021 Calyx S.A.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, api
+from odoo import models, api, fields
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     @api.multi
+    def _current_company_adaly(self):
+        for rec in self:
+            _logger.info("here")
+            _logger.info("USER: %s" % self.env.user.company_id.id)
+            if self.env.user.company_id.id == 18:
+                _logger.info("entro")
+                flag = True
+            else:
+                flag = False
+
+            rec.keep_company_adaly = flag
+
+    @api.multi
     def print_account_adaly_voucher(self):
         return self.env.ref(
             "account_invoice_adaly_report.action_custom_invoice_report"
         ).report_action(self)
+
+    keep_company_adaly = fields.Boolean(
+        string="Adaly", compute="_current_company_adaly"
+    )
